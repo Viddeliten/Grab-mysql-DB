0c45531739b3dca6c3c8ae91c5d234922c941a7d
vidde <johanna.julen@gmail.com>
2015-08-24 20:31:53 +0200

    some comments

--- config-sample.php
+++ config-sample.php
@@ -1,5 +1,5 @@
 <?php
 //Rename this file config.php and make desired changes below
-define('SERIALIZED_PATH',"../");
-define('MAIN_CONFIG_PATH', "..");
+define('SERIALIZED_PATH',"../"); // The db structure file will be put and fetched here
+define('MAIN_CONFIG_PATH', ".."); // The config.php with db connection values will be fetched here
 ?>
